ca919b602c793fdf0bafec6bb43d31f9f5e066a8
Joshua M. Clulow <jmc@oxide.computer>
2022-04-12 21:34:45 -0700

    XXX no workspace for generated crate files

--- progenitor/src/main.rs
+++ progenitor/src/main.rs
@@ -90,8 +90,7 @@ fn main() -> Result<()> {
                 \n\
                 [dependencies]\n\
                 {}\n\
-                \n\
-                [workspace]\n",
+                \n",
                 name,
                 version,
                 builder.dependencies().join("\n"),
